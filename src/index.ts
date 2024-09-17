f1c366c7cb1a0a2ef33afaed8e8c2d01e8551868
SirPedr <ppagbpedro@gmail.com>
2024-09-17 20:35:25 -0300

    feat: implementing time-based inactivity sessions

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { sessionIdMiddleware } from "./middleware/sessionId/sessionId.middleware
 type Environment = {
   UPSTASH_REDIS_URL: string;
   UPSTASH_REDIS_TOKEN: string;
+  SESSION_TTL: number;
 };
 
 const app = new Hono();
@@ -14,12 +15,20 @@ const app = new Hono();
 app.use(sessionIdMiddleware);
 
 app.get("/", async (c) => {
-  const { UPSTASH_REDIS_URL, UPSTASH_REDIS_TOKEN } = env<Environment>(c);
+  const { UPSTASH_REDIS_URL, UPSTASH_REDIS_TOKEN, SESSION_TTL } =
+    env<Environment>(c);
+
   const redis = await getRedis(UPSTASH_REDIS_URL, UPSTASH_REDIS_TOKEN);
 
   const sessionID = getSessionID(c);
+  const message = await redis.get<string | null>(sessionID);
 
-  await redis.set(sessionID, "Hello, World!");
+  if (!message) {
+    await redis.set(sessionID, "Hello, World!", { ex: SESSION_TTL });
+  }
+
+  redis.expire(sessionID, SESSION_TTL);
+
   return c.json({ id: sessionID });
 });
 
