167932031a3f441935c8ac622fffd6e9144c33b0
SirPedr <ppagbpedro@gmail.com>
2024-09-21 02:01:19 -0300

    feat: connecting to mongodb
    This was surpringly painful to get off the ground.

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@ import { env } from "hono/adapter";
 import { getRedis } from "./lib/getRedis";
 import { getSessionID } from "./lib/getSessionID";
 import { sessionIdMiddleware } from "./middleware/sessionId/sessionId.middleware";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 type Environment = {
   UPSTASH_REDIS_URL: string;
   UPSTASH_REDIS_TOKEN: string;
+  DATABASE_URL: string;
   SESSION_TTL: number;
 };
 
@@ -15,10 +18,17 @@ const app = new Hono();
 app.use(sessionIdMiddleware);
 
 app.get("/", async (c) => {
-  const { UPSTASH_REDIS_URL, UPSTASH_REDIS_TOKEN, SESSION_TTL } =
+  const { UPSTASH_REDIS_URL, UPSTASH_REDIS_TOKEN, SESSION_TTL, DATABASE_URL } =
     env<Environment>(c);
 
   const redis = await getRedis(UPSTASH_REDIS_URL, UPSTASH_REDIS_TOKEN);
+  const prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: DATABASE_URL,
+      },
+    },
+  }).$extends(withAccelerate());
 
   const sessionID = getSessionID(c);
   const message = await redis.get<string | null>(sessionID);
@@ -29,7 +39,10 @@ app.get("/", async (c) => {
 
   redis.expire(sessionID, SESSION_TTL);
 
-  return c.json({ id: sessionID });
+  const response = await prisma.profile.findMany();
+
+  await redis.set("collection", JSON.stringify(response.map((r) => r.id)));
+  return c.json({ profile: response[37] });
 });
 
 export default app;
