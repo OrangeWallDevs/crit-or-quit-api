5c221dd95233e8c81181499054ee87988c999d8b
SirPedr <ppagbpedro@gmail.com>
2024-10-13 01:21:28 -0300

    refactor: removing unused types

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,6 @@ import { Hono } from "hono";
 import { sessionIdMiddleware } from "./middleware/sessionId/sessionId.middleware";
 import { profileApp } from "./modules/profile";
 
-type Environment = {
-  UPSTASH_REDIS_URL: string;
-  UPSTASH_REDIS_TOKEN: string;
-  DATABASE_URL: string;
-  SESSION_TTL: number;
-};
-
 export const createApp = async () => {
   const app = new Hono();
 
